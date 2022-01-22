accc4bb5463653242e6232d51584757f671de6dd
Liora Haydont <loulounie@gmail.com>
2022-01-21 20:41:36 -0500

    Cards, Deck, Drag and movement

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,4 +5,31 @@ using UnityEngine;
 public class Card : MonoBehaviour
 {
     public bool day;
+    public float speed;
+    public GameObject square;
+    public Vector3 target;
+    private IEnumerator moveCoroutine;
+    private bool moving;
+
+    public void MoveTo(Vector3 position)
+    {
+        target = position;
+        if (moving)
+            StopCoroutine(moveCoroutine);
+        moveCoroutine = Move(position);
+        StartCoroutine(moveCoroutine);
+    }
+
+    private IEnumerator Move(Vector3 position)
+    {
+        moving = true;
+        while (Vector3.Distance(transform.position, position) > speed)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, position, speed);
+            yield return new WaitForEndOfFrame();
+        }
+        transform.position = position;
+        moving = false;
+    }
+
 }
---
+++ Assets/Scripts/DragManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragManager : MonoBehaviour
+{
+    public bool dragging = false;
+    public Vector3 offset;
+    public Card currentCard = null;
+    public Deck deck;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))//on pickup
+        {
+            Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, 10));
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0, LayerMask.GetMask("Draggable"));
+            if (hit.collider != null)
+            {
+                Card card = hit.collider.GetComponent<Card>();
+                if (card != null)
+                {
+                    currentCard = card;
+                    currentCard.GetComponent<SpriteRenderer>().sortingOrder = 100;
+                    currentCard.square.GetComponent<SpriteRenderer>().sortingOrder = 101;
+                    offset = card.transform.position - mousepos;
+                    deck.RemoveCard(card);
+
+                }
+            }
+
+        }else if (Input.GetMouseButton(0))//on drag
+        {
+            if(currentCard != null)
+            {
+                Vector3 cardPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+                cardPos.z = currentCard.transform.position.z;
+                currentCard.transform.position = cardPos;
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))//on release
+        {
+            if (!currentCard) return;
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0, LayerMask.GetMask("Droppable"));
+            if (hit.collider != null)
+            {
+                if (hit.collider.GetComponent<Deck>())
+                {
+                    deck.AddCard(currentCard);
+                }
+                else
+                {
+                    currentCard.MoveTo(hit.collider.transform.position);
+                }
+            }
+            else
+            {
+                deck.AddCard(currentCard);
+            }
+            currentCard = null;
+        }
+
+
+
+    }
+}
